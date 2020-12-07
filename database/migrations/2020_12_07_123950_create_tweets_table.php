f4383c4d6c81a87480c4b8a26fbdd35020353a6f
Manash <manashbharali79@gmail.com>
2020-12-08 00:51:39 +0530

    Added the tweet resource

---
+++ database/migrations/2020_12_07_123950_create_tweets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->string('tweet_title');
+            $table->mediumText('tweet_body');
+            $table->string('tweet_author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
---
+++ tests/Feature/TweetManagementTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use App\Models\Tweet;
+
+class TweetManagementTest extends TestCase
+{	
+	use RefreshDatabase;
+	/** @test */ 
+	public function a_tweet_can_be_added()
+	{	
+		// $this->withoutExceptionHandling();
+
+		$response = $this->post('/tweets', [
+			'tweet_title' => 'My first tweet',
+			'tweet_body' => 'Yay! I can post whatever i want',
+			'tweet_author' => 'John Doe',
+		]);
+
+		$response->assertOk();
+		$this->assertCount(1, Tweet::all());
+	}
+	
+	/** @test */
+	public function a_tweet_title_is_required()
+	{
+
+		// $this->withoutExceptionHandling();
+		
+		$response = $this->post('/tweets', [
+			'tweet_title' => '',
+			'tweet_body' => 'Yay! I can post whatever i want',
+			'tweet_author' => 'John Doe',
+		]);
+		$response->assertSessionHasErrors('tweet_title');
+	}
+
+		/** @test */
+		public function a_tweet_body_is_required()
+		{
+			// $this->withoutExceptionHandling();
+			$response = $this->post('/tweets', [
+				'tweet_title' => 'My tweet tile',
+				'tweet_body' => '',
+				'tweet_author' => 'John Doe',
+			]);
+			$response->assertSessionHasErrors('tweet_body');
+		}
+
+		/** @test */
+		public function a_tweet_author_is_required()
+		{
+			// $this->withoutExceptionHandling();
+			$response = $this->post('/tweets', [
+				'tweet_title' => 'My tweet tile',
+				'tweet_body' => 'Yay! I can post whatever i want',
+				'tweet_author' => '',
+			]);
+			$response->assertSessionHasErrors('tweet_author');
+		}
+
+		/** @test */ 
+		public function a_tweet_can_be_updated()
+		{	
+
+			$this->post('/tweets', [
+				'tweet_title' => 'My first tweet',
+				'tweet_body' => 'Yay! I can post whatever i want',
+				'tweet_author' => 'John Doe',
+			]);
+
+			$tweet = Tweet::first();
+
+			$response = $this->patch('/tweets/'.$tweet->id, [
+				'tweet_title' => 'First tweet',
+				'tweet_body' => 'I can post whatever i want',
+				'tweet_author' => 'John Doe',
+			]);
+
+			$this->assertEquals('First tweet', Tweet::first()->tweet_title);
+			$this->assertEquals('I can post whatever i want', Tweet::first()->tweet_body);
+			$this->assertEquals('John Doe', Tweet::first()->tweet_author);
+
+		}
+
+}
