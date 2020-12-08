be785c7ed5fdfe61649cf5de1cde80335c0de31a
Manash <manashbharali79@gmail.com>
2020-12-09 01:11:49 +0530

    Route updated for Authors

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TweetsControllers;
+use App\Http\Controllers\TweetsController;
+use App\Http\Controllers\AuthorsController;
 
-Route::post('/tweets', [TweetsControllers::class, 'store'])->name('tweet.store');
-Route::patch('/tweets/{tweet}', [TweetsControllers::class, 'update'])->name('tweet.update');
+Route::post('/tweets', [TweetsController::class, 'store'])->name('tweet.store');
+Route::patch('/tweets/{tweet}', [TweetsController::class, 'update'])->name('tweet.update');
+Route::delete('/tweets/{tweet}', [TweetsController::class, 'destroy'])->name('tweet.delete');
+
+
+Route::post('/authors', [AuthorsController::class, 'store'])->name('author.store');
