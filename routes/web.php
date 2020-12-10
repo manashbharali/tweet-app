9f23aa75a33afc97c653654c80e71efc3a9aaa81
Manash <manashbharali79@gmail.com>
2020-12-10 14:49:32 +0530

    laravel-breeze added

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TweetsController;
 
-Route::post('/tweets', [TweetsController::class, 'store'])->name('tweet.store');
-Route::patch('/tweets/{tweet}', [TweetsController::class, 'update'])->name('tweet.update');
-Route::delete('/tweets/{tweet}', [TweetsController::class, 'destroy'])->name('tweet.delete');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
